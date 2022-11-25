7175996c9d440bf1c365cf13a433d0bd418d9535
Dominic Seithel <seitheldominic@gmail.com>
2022-11-25 11:08:22 +0100

    working frame

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import { BootScene } from './scenes/boot-scene';
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Clocks',
+  url: 'https://github.com/digitsensitive/phaser3-typescript',
+  version: '1.0',
+  type: Phaser.AUTO,
+  scene: [BootScene],
+  input: {
+    keyboard: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    parent: 'game',
+    width: 640,
+    height: 960
+  },
+  backgroundColor: 0x4ac7ff
+};
