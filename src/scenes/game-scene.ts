d19bde68ef65e084fbd7e4bb7bc7d5aea952a3f0
Dominic Seithel <seitheldominic@gmail.com>
2022-12-02 11:53:04 +0100

    menu and draw fix

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -14,6 +14,23 @@ export class GameScene extends Phaser.Scene {
     scrollHelper: null,
     cursors: null,
   };
+  private menu: {
+    bar: Phaser.GameObjects.Rectangle,
+    buttons: {
+      check: {rect: Phaser.GameObjects.Rectangle, text: Phaser.GameObjects.Text, symbol: Phaser.GameObjects.Sprite},
+      undo: {rect: Phaser.GameObjects.Rectangle, text: Phaser.GameObjects.Text, symbol: Phaser.GameObjects.Sprite},
+      help: {rect: Phaser.GameObjects.Rectangle, text: Phaser.GameObjects.Text, symbol: Phaser.GameObjects.Sprite},
+      cross: {rect: Phaser.GameObjects.Rectangle, text: Phaser.GameObjects.Text, symbol: Phaser.GameObjects.Sprite},
+    },
+  } = {
+    bar: null,
+    buttons: {
+      check: {rect: null, text: null, symbol: null},
+      undo: {rect: null, text: null, symbol: null},
+      help: {rect: null, text: null, symbol: null},
+      cross: {rect: null, text: null, symbol: null},
+    },
+  }
 
   constructor() {
     super({
@@ -23,6 +40,7 @@ export class GameScene extends Phaser.Scene {
 
   create(): void {
     this.draw();
+    this.createMenu();
     this.gamestate.scrollHelper = this.physics.add.sprite(0, Constants.height / 2, '').setScale(0);
     this.cameras.main.startFollow(this.gamestate.scrollHelper, true, 0.5, 0.5);
     this.gamestate.scrollHelper.setCollideWorldBounds(true);
@@ -36,7 +54,7 @@ export class GameScene extends Phaser.Scene {
     else this.gamestate.scrollHelper.setVelocityY(0);
   }
 
-  private selection(element: any, x: number, y: number) {
+  private selection(element: any, x: number, y: number): void {
     // future feature TODO's:
     // Custom Color Support
     // Neighbor Highlighting
@@ -56,6 +74,12 @@ export class GameScene extends Phaser.Scene {
   }
 
   private draw(): void {
+    for(let i = 0; i < this.gamestate.elements.length; i++) {
+      for(let j = 0; j < this.gamestate.elements[i].length; j++) {
+        this.gamestate.elements[i][j].number.destroy();
+        this.gamestate.elements[i][j].rect.destroy();
+      }
+    }
     this.gamestate.elements = [];
     for(let i = 0; i < this.gameboard.board.length; i++) {
       this.gamestate.elements.push([]);
@@ -67,6 +91,8 @@ export class GameScene extends Phaser.Scene {
             rect: this.add.rectangle(x, y, 50, 30, 0x000000), 
             number: this.add.text(x - 7, y - 12, this.gameboard.board[i][j].value.toString(), { fontFamily: 'monospace', color: '#00ff00', fontSize: '25px'}),
           });
+          this.gamestate.elements[i][j].rect.depth = 1;
+          this.gamestate.elements[i][j].number.depth = 2;
           this.gamestate.elements[i][j].rect.setInteractive();
           this.gamestate.elements[i][j].rect.on('pointerup', (element: any) => this.selection(element, j, i));
         } else {
@@ -74,10 +100,19 @@ export class GameScene extends Phaser.Scene {
             rect: this.add.rectangle(x, y, 50, 30, 0x999999), 
             number: this.add.text(x - 7, y - 12, this.gameboard.board[i][j].value.toString(), { fontFamily: 'monospace', color: '#000000', fontSize: '25px'}),
           });
+          this.gamestate.elements[i][j].rect.depth = 1;
+          this.gamestate.elements[i][j].number.depth = 2;
         }
       }
     }
     this.cameras.main.setBounds(0, 0, Constants.width, this.gameboard.board.length * 30);
     this.physics.world.setBounds(0, Constants.height / 2, Constants.width, this.gameboard.board.length * 30 - Constants.height);
+  }
+
+  private createMenu(): void {
+    this.menu.bar = this.add.rectangle(0, Constants.height - 50, Constants.width, 50, 0x000000);
+    this.menu.bar.setScrollFactor(0);
+    this.menu.bar.setScale(2);
+    this.menu.bar.depth = 10;
   }
 }
